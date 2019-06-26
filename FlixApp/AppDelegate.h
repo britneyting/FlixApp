1bb50bb34f313fb08c52fa6457304fb3af05ccfd
Britney Ting <britneyting@fb.com>
2019-06-26 11:25:07 -0700

    Initial Commit

---
+++ FlixApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlixApp
+//
+//  Created by britneyting on 6/26/19.
+//  Copyright © 2019 britneyting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
